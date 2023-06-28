6e186c37001ce9a01e8e4e42685e3d403040b88b
Wilson Ng <wilsonngja@gmail.com>
2023-06-28 20:14:12 +0800

    Include boxSize for small screen

--- src/routing/AboutMe.tsx
+++ src/routing/AboutMe.tsx
@@ -73,10 +73,7 @@ const AboutMe = () => {
               borderWidth="4px"
               borderStyle="solid"
               borderColor="orange.200"
-              height={isSmallScreen ? "10%" : "auto"}
-              width="auto"
-              marginY="0px"
-              marginX="auto"
+              boxSize={isSmallScreen ? "100px" : "220px"}
             />
           </GridItem>
           <GridItem>
